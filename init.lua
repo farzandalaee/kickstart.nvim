9c7d9d0bec098554b4082f998e8c4cf0f08356b5
Farzan Dalaee <farzan.dalaee@gmail.com>
2025-08-09 16:14:37 +0330

    add floating terminal plugin

--- init.lua
+++ init.lua
@@ -168,6 +168,7 @@ vim.o.confirm = true
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('i', 'jk', '<Esc>')
+vim.keymap.set('i', '<Leader>xx', ':so %<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
