07ae980b0d54e90fadf746894b9417cca37353f0
Farzan Dalaee <farzan.dalaee@gmail.com>
2025-08-10 18:45:49 +0330

    add outline

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -73,4 +73,16 @@ return {
       vim.notify = require 'notify'
     end,
   },
+  -- outline
+  {
+    'hedyhli/outline.nvim',
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+
+      require('outline').setup {
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
+  },
 }
